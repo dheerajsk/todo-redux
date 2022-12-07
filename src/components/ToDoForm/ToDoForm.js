7b7d190c7a88ce5d1af360a401108508ad88104e
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-12-07 22:14:58 +0530

    to do app

---
+++ src/components/ToDoForm/ToDoForm.js
@@ -0,0 +1,14 @@
+function ToDoForm() {
+  return (
+    <div>
+      <form>
+        <input type="text" className="form-control" />
+        <button type="submit" className="float-end mt-2 btn btn-success">
+          Add
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default ToDoForm;
---
+++ src/components/ToDoList/ToDoList.js
@@ -0,0 +1,13 @@
+import "./ToDoList.css";
+
+function ToDoList() {
+  return (
+    <ul className="list">
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  );
+}
+
+export default ToDoList;
